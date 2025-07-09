ee9cee9a4e539e205f9e55a59986579ae5c80df4
Smit Maurya <smitmaurya2004@gmail.com>
2025-07-09 23:16:05 +0530

    testing deploymenet

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://youtubenotebooks.onrender.com', // Backend server URL
+        target: 'http://localhost:8000', // Backend server URL
         changeOrigin: true,
         secure: false,
         // *** THIS IS THE KEY CHANGE ***
